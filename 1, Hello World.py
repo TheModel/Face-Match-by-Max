15da544cfaa63c526f0e630f7141caddef2c272a
TheModel <152254955+TheModel@users.noreply.github.com>
2023-12-13 11:45:24 -0800

    Update 1, Hello World.py

--- 1, Hello World.py
+++ 1, Hello World.py
@@ -1,3 +1,14 @@
 # This program prints hello world on the console screen
 
 print("Hello World!")
+
+
+number = int(input("Enter a number: "))
+if number > 0:
+    print("The number entered is greater than 0")
+elif number < 0:
+    print("The number entered is less than 0")
+else:
+    print("The number entered is 0")
+
+
